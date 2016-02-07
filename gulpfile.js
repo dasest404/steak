878f6e60df2eca737d0ebf5b3db3010709c45a59
Nick <parsnick@users.noreply.github.com>
2016-02-07 22:01:02 +0000

    Remove unused phpspec dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,5 @@
 var gulp = require('gulp');
-var phpspec = require('gulp-phpspec');
 var phpunit = require('gulp-phpunit');
-
-gulp.task('phpspec', () => {
-    gulp.src('phpspec.yml')
-        .pipe(phpspec());
-});
 
 gulp.task('phpunit', () => {
     gulp.src('phpunit.xml')
@@ -13,6 +7,5 @@ gulp.task('phpunit', () => {
 });
 
 gulp.task('tdd', () => {
-    gulp.watch(['src/**/*.php', 'spec/**/*.php'], ['phpspec']);
     gulp.watch(['src/**/*.php', 'tests/**/*.php'], ['phpunit']);
 });
