db162c7e8eb3f2d3d692fbf5114ce3558ede6e00
Nick <parsnick@users.noreply.github.com>
2016-02-09 18:10:45 +0000

    Moved tests config and gulpfile to tests/fixtures

--- tests/fixtures/gulpfile.js
+++ tests/fixtures/gulpfile.js
@@ -6,9 +6,13 @@ var gulp = require('gulp');
 var filter = require('gulp-filter');
 var print = require('gulp-print');
 
+/**
+ * Configurable paths
+ */
 var paths = {
     source: argv.source,
     output: argv.dest,
+    serveRelative: argv.subdir,
     patterns: {
         blade: [
             `${argv.source}/**/*.php`,
@@ -26,29 +30,24 @@ var paths = {
     }
 };
 
+/**
+ * Publish static files from the source directory.
+ */
 gulp.task('steak:publish', () => {
     return gulp
         .src(paths.patterns.not.blade)
         .pipe(changed(paths.output))
-        .pipe(filter(isFile))
+        .pipe(filter(file => ! file.isDirectory()))
         .pipe(print())
         .pipe(gulp.dest(paths.output))
     ;
 });
 
+/**
+ * Start browserSync server and watch for source changes.
+ */
 gulp.task('steak:serve', () => {
-    browserSync.init({
-        server: {
-            baseDir: paths.output,
-            routes: {
-                "/steak": paths.output
-            }
-        },
-        files: [
-            paths.output
-        ],
-        startPath: "/steak"
-    });
+    browserSync.init(getBrowserSyncConfig());
 
     gulp.watch(paths.patterns.not.blade, ['steak:publish']);
 
@@ -57,6 +56,32 @@ gulp.task('steak:serve', () => {
     });
 });
 
-var isFile = function (file) {
-    return ! file.isDirectory();
+
+/**
+ * Generate browserSync configuration.
+ *
+ * @returns {{server: {baseDir: *}, files: *[]}}
+ */
+var getBrowserSyncConfig = function () {
+    // Create a server in our build folder, and
+    // watch the build folder for changes
+    var config = {
+        server: {
+            baseDir: paths.output
+        },
+        files: [
+            paths.output
+        ]
+    };
+
+    // By default, sites are served from the root.
+    // gh-pages sites exist in a /<projectName> subdirectory.
+    // To emulate this, the --subdir option can be used.
+    if (paths.serveRelative) {
+        config.server.routes = {};
+        config.server.routes['/'+paths.serveRelative] = paths.output;
+        config.startPath = '/'+paths.serveRelative;
+    }
+
+    return config;
 };
